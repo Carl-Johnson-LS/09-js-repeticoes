7312342503e2d55d743073f89762513eee32c88a
Ronan Mendes Nogueira Couto <rmnc@aluno.ifnmg.edu.br>
2023-07-02 21:06:14 -0300

    Corrigindo js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-function contar () {
+function contar() {
     let ini = document.getElementById('txti')
     let fim = document.getElementById('txtf')
     let passo = document.getElementById ('txtp')
     let res = document.getElementById('res')
   
-  
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
       res.innerHTML = 'impossível de contar!'
-      //window.alert('[ERRO] Faltam dados')
     } else {
       res.innerHTML = 'Contando: <br>'
       let i = Number(ini.value)
@@ -19,15 +17,14 @@ function contar () {
       }
       if (i < f) {
         for (let c = i; c <= f; c+= p) {
-  
-  
+            res.innerHTML += ` ${c} \u{1F449}`
         }
       } else {
         for(let c = i; c >= f; c -= p) {
-          res.innerHTML += `${c} \u{1f449}`
+          res.innerHTML += `${c} \u{1F449}`
         }
       }
-      res.innerHTML += `\u{1f3c1}`
+      res.innerHTML += `\u{1F3C1}`
     }
   }
   
